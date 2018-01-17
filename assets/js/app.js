dcbaafb144692f296cf5a5939366a1f7df227981
Katherine <krosenk729@gmail.com>
2018-01-16 19:45:20 -0500

    cleaning up

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,190 +1,330 @@
-// (function(){
-	// Initialize Firebase
-	// Temp UID : dUXuGqt7Y4YzxOUFSzMqnyPTlIJ2 (katherine (9L))
-	// https://console.firebase.google.com/u/0/project/recurring-payments-tracker/authentication/providers
-	// https://console.developers.google.com/apis/credentials?project=recurring-payments-tracker&authuser=0
-	var config = {
+/* Initialize
+// Declaring variables
+// Setting up firebase config
+*/
+const CONFIG = {
 		apiKey: "AIzaSyD6FBfO14EljQNaKSud9lHWlsasUita6uY",
 		authDomain: "recurring-payments-tracker.firebaseapp.com",
 		databaseURL: "https://recurring-payments-tracker.firebaseio.com",
 		projectId: "recurring-payments-tracker",
 		storageBucket: "recurring-payments-tracker.appspot.com",
 		messagingSenderId: "428623070633"
-	};
-	firebase.initializeApp(config);
+	},
+	PROVIDER = new firebase.auth.GoogleAuthProvider(),
+	FUTUREYEAR = '2020-01-01';
 
-	let fbProvder = new firebase.auth.GoogleAuthProvider(),
-		fbAuth = firebase.auth(), 
-		dbRef = firebase.database(),
-		usrRef = dbRef.ref('users'),
-		payRef = dbRef.ref('payments'), //dbRef.ref('payments').orderByChild('user').equalTo(user.uid)
-		userContext = '',
-		runningTotal = 0; //cost of all payments
+firebase.initializeApp(CONFIG);
+
+let currentUser = '',
+	countUnit = 'days',
+	recheckInt = {};
+
+/* Auth Setup
+// Listen for sign in / out
+// Listen for auth changes
+*/
+$('.bt-user-login, .placeholder-for-list a').click(signUserIn);
+$('.bt-user-logout').click(signUserOut);
+
+function signUserIn() {
+	firebase.auth().signInWithPopup(PROVIDER)
+	.then(function(result) {
+		console.log('user sign in triggered', result);
+	}).catch(function(error) {
+	  	console.log('error on sign in trigger', error);
+	});
+}
+function signUserOut(){
+	firebase.auth().signOut()
+	.then(function(){
+		clearForm();
+		firebase.database().ref('payments').off('value');
+		firebase.database().ref('payments').off('child_added');
+		firebase.database().ref('payments').off('child_changed');
+		// signed out -- all UI updates handled by onAuthStateChanged
+	});
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if(user){
+    currentUser = {
+    	name: user.displayName || user.email, 
+    	id: user.uid,
+    	img: user.photoURL
+    };
+    firebase.database().ref('users').child(currentUser.id).update({
+    	id: currentUser.id
+    });
+
+	function switchUItoSignedIn(u){
+		$('.bt-user-login, .placeholder-for-list').hide();
+		$('.user-nm').text(u.name);
+		$('.user-img').attr('src', u.img);
+		$('.user-loggedin, .payments-disp, .payments-form').show();
+	}
+	switchUItoSignedIn(currentUser);
+	onlyIfSignedIn(currentUser);
+    console.log('sign in complete', user);
+
+  } else {
+    console.log('signed out / no user');
+    currentUser = undefined;
+
+	function switchUItoSignedOut(){
+		$('.bt-user-login, .placeholder-for-list').show();
+		$('.user-nm').text('');
+		$('.user-img').attr('src', '');
+		$('.user-loggedin, .payments-disp, .payments-form').hide();
+		$('.payments-items tr').remove(); // for safety
+	}
+	switchUItoSignedOut();
+  }
+});
+
+/* Once a User Signs In
+// Listen for 'value' event
+// Listen for user add/remove/update values
+// Listen for user interactions (add new, update, preference change)
+*/
+
+function onlyIfSignedIn(user){
+	firebase.database().ref('payments').orderByChild('user').equalTo(user.id)
+		.on('child_changed', function(data){
+			showPayments(data);
+		});
+
+	firebase.database().ref('payments').orderByChild('user').equalTo(user.id)
+		.on('child_added', function(data){
+			showPayments(data);
+		});
+
+	firebase.database().ref('payments').orderByChild('user').equalTo(user.id)
+		.on('child_removed', function(data){
+			unshowPayments(data);
+		});
+
+	recheck = setInterval(recheckCountdown, countUnit === 'days' ? 86400000 : 60000 );
 
 	$('.btn-add-new').on('click', sendNewPayment);
 	$('.payments-items').on('change', 'input, select', updatePayment);
-	$('.payments-items').on('click', 'btn-remove-row', delPayment);
+	$('.payments-items').on('click', '.btn-remove-row', delPayment);
 
-	$('.bt-user-login').click(signUserIn);
-	$('.bt-user-logout').click(signUserOut);
-
-	payRef.on('child_changed', function(data, data2){
-		// console.log('child_changed', data, data2);
-		showPayments(data);
+	$('#hidetotal-true').on('change', function(){
+		$('#should-hide-money').html('.show-money{visibility: hidden !important}');
 	});
-	payRef.on('child_added', function(data){
-		// console.log('child_added', data);
-		showPayments(data);
+	$('#hidetotal-false').on('change', function(){
+		$('#should-hide-money').empty();
 	});
-	payRef.on('child_removed', function(data){
-		console.log(data);
-	});
+}
 
-	fbAuth.onAuthStateChanged(function(user){
-		if(user){
-			$('.payments-list').show();
-			$('.placeholder-for-list').hide();
-			$('.user-img').attr('src', user.photoURL);
-			$('.user-nm').text(user.displayName);
-			setUserContext(user);
-		}else{
-			$('.payments-list').hide();
-			$('.placeholder-for-list').show();
-		}
-	});
+/* Form Method: sendNewPayment
+// Create a new payment in firebase
+// When a button within a form is clicked 
+*/
+function sendNewPayment(){
 
-	function signUserIn(){
-		let provider = new firebase.auth.GoogleAuthProvider();
-		fbAuth.signInWithPopup(provider)
-		.then(function(user){
-			let p = 'users/' + user.uid,
-				o = {uid: user.uid};
-			updateFirebase(o, p, 'update');
-		})
-		.error(function(err){
-			console.log('error on signUserIn', err);
-		});
-	}
+	let p = 'payments',
+	o = {
+		store: $('#new-store').val().trim(),
+		desc: '',
+		cost: $('#new-cost').val().trim(),
+		freqUnit: $('#new-freqUnit').val().trim(),
+		firstEvntDay: $('#new-firstEvntDay').val().trim(),
+		firstEvntTime: $('#new-firstEvntTime').val().trim(),
+		user: currentUser.id
+	};
 
-	function signUserOut(){
-		fbAuth.signOut();
-	}
+	// Send New Payment to Firebase (push)
+	updateFirebase(p, o, 'push').then(()=>{ clearForm(); });
+}
 
-	function setUserContext(user){
-		//Get only payments associated with a user
-		userContext = user.uid;
-		payRef = .ref('payments').orderByChild('user').equalTo(user.uid);
-		
-	}
+/* Field Method: updatePayment
+// Sends an object to firebase as an update
+// When field is blurred or changed 
+*/
+function updatePayment(){
+	let p = 'payments/' + $(this).parentsUntil('tbody')[1].attributes["data-id"].value,
+		o = {};
+	//Send updated values to firebase
+	o[$(this).attr('name')] = $(this).val();
+	updateFirebase(p, o, 'update');
+}
 
-	function delPayment(){
-		console.log( 'del is not written yet', this );
-	}
 
-	/* Function to update only one field when inputs updated */
-	function updatePayment(){
-		let p = 'payments/' + $(this).parentsUntil('tbody')[1].attributes["data-id"].value,
-			o = {};
+/* Field Method: delPayment
+// Sends a delete to firebase for a payment 
+// And removes from user reference 
+// When remove is clicked for a row 
+*/
+function delPayment(){
+	let pID =  $(this).parentsUntil('tbody')[1].attributes["data-id"].value;
 
-		//Send updated values to firebase
-		o[$(this).attr('name')] = $(this).val();
-		updateFirebase(o, p, 'update');
-	}
+	// Remove payment and parent reference to payment 
+	updateFirebase('payments/' + pID, {}, 'remove');
+	updateFirebase('users/' + currentUser.id + '/payments/' + pID, {}, 'remove');
+}
 
-	/* Function to update Firebase when a payment is added and clear form out */
-	/* Optionally takes in a childID if this is a full update for some unknown reason */
-	function sendNewPayment(){
-		console.log('sendNewPayment was called');
+/* Helper Function: clearForm
+// Resets form fields to default values
+*/
+function clearForm(){
+	$('#new-store, #new-cost').val('');
+	$('#new-firstEvntTime').val('12:00');
+	$('#new-firstEvntDay').val( moment(new Date).format('YYYY-MM-DD') );
+	$('#new-freqUnit').val('year');
+}
 
-		let r = 'payments',
-		p = {
-			store: $('#new-store').val().trim(),
-			desc: '',
-			cost: $('#new-cost').val().trim(),
-			freqUnit: $('#new-freqUnit').val().trim(),
-			firstEvntDay: $('#new-firstEvntDay').val().trim(),
-			firstEvntTime: $('#new-firstEvntTime').val().trim()
-		};
-
-		// Send New Payment to Firebase (push)
-		updateFirebase(p, r);
-
-		//Reset Form
-		$('#new-store, #new-cost').val('');
-		$('#new-firstEvntTime').val('12:00');
-		$('#new-firstEvntDay').val( moment().format('YYYY-MM-DD') );
-		$('#new-freqUnit').val('Year');
-	}
-
-	/* Function to update Firebase */
-	function updateFirebase(o, path = 'payments', t = 'push'){
-		console.log('updateFirebase was called', o,path);
-		if(t === 'update'){
+/* Helper Function: updateFirebase
+// Sends an object to firebase at a given path with a given operation
+*/
+function updateFirebase(path = 'payments', o = {}, t = 'push'){
+	console.log('updateFirebase was called', o, path);
+	switch(t){
+		case 'update':
 			firebase.database().ref(path).update(o);
-		} else{
+			break;
+		case 'remove':
+			firebase.database().ref(path).remove();
+			break;
+		case 'push':
+		default:
 			firebase.database().ref(path).push(o);
-		}
+			break;
+	}
+}
+
+/* Helper Function: showPayments
+// Accepts an object which represents the return value from a firebase event callback
+// Creates an html fragment and appends/replaces a row in the payments table
+*/
+function showPayments(data){
+	let ppath = data.ge.path.n[1],
+		prow = $('tbody').find(`tr[data-id="${ppath}"]`),
+		pstore = data.val().store,
+		pdesc = data.val().desc,
+		pcost = data.val().cost,
+		pfreqUnit = data.val().freqUnit,
+		pfirstEvntDay = data.val().firstEvntDay,
+		pfirstEvntTime = data.val().firstEvntTime,
+		pcosttoFuture = formatNumber((countdownTo(pfirstEvntDay, pfirstEvntTime, FUTUREYEAR, pfreqUnit)+1)*pcost),
+		pcounttonext = formatNumber(Math.abs(countdownTo(pfirstEvntDay, pfirstEvntTime, '', countUnit))) + ' ' + countUnit; 
+	
+	let pfragment = 
+	`<tr data-id="${ppath}">
+	<td>
+		<label class="sr-only">Description of Payment for Store</label>
+		<input type="text" name="desc" value="${pdesc || pstore}" required>
+		<small class="desc-text">Store: ${pstore}</small>
+	</td>
+	<td>
+		<label class="sr-only">Cost of Recurring Payment</label>
+		$<input type="number" name="cost" value="${pcost}">
+		<select name="freqUnit" value="${pfreqUnit}" required>
+			<option value="hour" ${pfreqUnit==="hour" ? 'selected' : ''}>Hourly</option>
+			<option value="day" ${pfreqUnit==="day" ? 'selected' : ''}>Daily</option>
+			<option value="week" ${pfreqUnit==="week" ? 'selected' : ''}>Weekly</option>
+			<option value="month" ${pfreqUnit==="month" ? 'selected' : ''}>Monthly</option>
+			<option value="year" ${pfreqUnit==="year" ? 'selected' : ''}>Yearly</option>
+		</select>
+		<small class="cost-until show-money">Total: $${pcosttoFuture} by ${moment(FUTUREYEAR).format('YYYY')}</small>
+	</td>
+	<td>
+		<label class="sr-only">Next Charge Date and Time</label>
+		<input type="date" name="firstEvntDay" min="${moment(new Date).format('YYYY-MM-DD')}" max="${FUTUREYEAR}" value="${pfirstEvntDay}" required>
+		at 
+		<input type="time" step="60" name="firstEvntTime" value="${pfirstEvntTime}" required>
+		<small class="count-until">Next charge in ${pcounttonext}</small>
+	</td>
+	<td>
+		<button class="btn btn-remove-row">Remove</button>
+	</td>
+	</tr>`;
+	if(prow.length > 0){
+		prow.replaceWith(pfragment);
+		console.log('replacing');
+	} else {
+		$('.payments-items').append(pfragment);
+	}
+	calcTotal();
+}
+
+
+/* Helper Function: calcTotal
+// calculates total of all a users' payments
+// And shows in a total row  
+*/
+function calcTotal(){
+	let total = 0,
+		all = $('.payments-items input[name="cost"]');
+
+	for(let i=0, x = all.length; i < x; i++){
+		total += Number(all[i].value);
 	}
 
-	/* Function to update UI with a new payment row when a firebase child is added */
-	function showPayments(data){
-		let ppath = data.ge.path.n[1],
-			prow = $('tbody').find(`tr[data-id="${ppath}"]`);
-			pstore = data.val().store,
-			pdesc = data.val().desc,
-			pcost = data.val().cost,
-			pfreqUnit = data.val().freqUnit,
-			pfirstEvntDay = data.val().firstEvntDay,
-			pfirstEvntTime = data.val().firstEvntTime,
-			pcostto2020 = countdownTo(pfirstEvntDay, pfirstEvntTime, '2020-01-01', pfreqUnit)*pcost,
-			pcounttonext = Math.abs(countdownTo(pfirstEvntDay, pfirstEvntTime, '', 'days')) + ' days'; 
-			//alternative option of passing in pfreqUnit here
-		
-		let pfragment = 
-		`<tr data-id="${ppath}">
-		<td>
-			<label class="sr-only">Description of Payment for Store</label>
-			<input type="text" name="desc" value="${pdesc || pstore}">
-			<small class="desc-text text-muted">Store: ${pstore}</small>
-		</td>
-		<td>
-			<label class="sr-only">Cost of Recurring Payment</label>
-			$<input type="number" name="cost" value="${pcost}">
-			<small class="count-until text-muted">Total: $${pcostto2020} by 2020</small>
-		</td>
-		<td>
-			<label>Every</label>
-				<select name="freqUnit" value="${pfreqUnit}">
-				<option data-unit="Day" ${pfreqUnit==="Day" ? 'selected' : ''}>Day</option>
-				<option data-unit="Week" ${pfreqUnit==="Week" ? 'selected' : ''}>Week</option>
-				<option data-unit="Month" ${pfreqUnit==="Month" ? 'selected' : ''}>Month</option>
-				<option data-unit="Year" ${pfreqUnit==="Year" ? 'selected' : ''}>Year</option>
-			</select>
-		</td>
-		<td>
-			<label class="sr-only">Next Charge Date and Time</label>
-			<input type="date" name="firstEvntDay" min="2018-01-01" max="2020-01-01" value="${pfirstEvntDay}">
-			at 
-			<input type="time" step="60" name="firstEvntTime" value="${pfirstEvntTime}">
-			<small class="count-until text-muted">Next charge in ${pcounttonext}</small>
-		</td>
-		<td>
-			<button class="btn btn-remove-row">Remove</button>
-		</td>
-		</tr>`;
-		if(prow.length > 0){
-			prow.replaceWith(pfragment);
-			console.log('replacing');
+	$('.payments-totals td:nth-child(2)').text(total);
+}
+
+
+/* Helper Function: unshowPayments
+// Opposite of showPayments
+// Accepts an object which represents the return value from a firebase remove event 
+// Destroys the table row corresponding to the object
+*/
+function unshowPayments(data){
+	let ppath = data.ge.path.n[1],
+		prow = $('tbody').find(`tr[data-id="${ppath}"]`);
+	prow.remove();
+	calcTotal();
+}
+
+/* Helper Function: recheckCountdown
+// Checks if a payment date has passed 
+// If a date has passed, it is progressed to the next event date
+// If a date has not passed, the countdown time until next event is updated
+*/
+function recheckCountdown(countUnit){
+	console.log('countunit', countUnit);
+	let all = $('.payments-items tr');
+	// goal: get the firstEvntDay, firstEvntTime and tr
+	for(let i = 0, x=all.length; i < x; i++){
+		let trow = all[i],
+			tfirstEvntDay = $(trow).find('input[name="firstEvntDay"]').val(),
+			tfirstEvntTime = $(trow).find('input[name="firstEvntTime"]').val(),
+			tdiff = countdownTo(tfirstEvntDay, tfirstEvntTime, '', 'seconds');
+
+		if(tdiff > 0){
+			let tnewDate = moment(tfirstEvntDay + ' ' + tfirstEvntTime).add(1, $(trow).find('select[name="freqUnit"]').val()),
+				p = 'payments/'+ trow.dataset.id,
+				o = {};
+				o.firstEvntDay = tnewDate.format('YYYY-MM-DD');
+				o.firstEvntTime = tnewDate.format('HH:mm');
+			updateFirebase(p, o, 'update');
 		} else {
-			$('.payments-items').append(pfragment);
+			$(trow).find('.count-until').text('Next charge in '
+				+ Math.abs(countdownTo(tfirstEvntDay, tfirstEvntTime, '', countUnit))
+				+ ' ' + countUnit);
 		}
 	}
+}
 
-	/* Function to calculate the time between a day/time */
-	/* and another date (if provided) in specified units */
-	function countdownTo( nextEventDay, nextEventTime, untilDate = '', unit = 'days' ){
-		return moment((untilDate ? moment(untilDate) : moment()))
-		.diff( nextEventDay + ' ' + nextEventTime , unit);
-	}
+/* Helper Function: coundtownTo
+// Returns the time between a day, time and passed date
+*/
+function countdownTo( nextEventDay, nextEventTime, untilDate, unit = 'days'){
+	return moment(untilDate || new Date()).diff( nextEventDay + ' ' + nextEventTime , unit);
+}
 
-// })();
+/* Helper Function: formatNumber
+// Returns a number formatted with commas 
+// Accounts for scientific notation numbers
+*/
+function formatNumber(n){
+	let newN = '';
+	 do{
+	   let a = n%10;
+	   n = Math.trunc(n/10);
+	   newN = a+newN;
+	 }while(n>0)
+
+	return newN.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+}
